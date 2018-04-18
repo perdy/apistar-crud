f4dd38a88238b9d4b71cf983062c32689288b0cc
José Antonio Perdiguero <perdy.hh@gmail.com>
2018-04-18 12:59:16 +0200

    Remove pip dependency from setup.py

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,4 +1,11 @@
 Changes
 =======
+v0.2.1 - 2018-04-18
+ * Remove pip dependency from setup.py.
+
+v0.2.0 - 2018-04-14
+ * Upgraded for apistar v0.4.
+ * Usage of different types for input and output.
+
 v0.1.0 - 2018-04-09
  * Initial release.
--- setup.py
+++ setup.py
@@ -1,84 +1,99 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import os
-import sys
+import re
 
-from pip.download import PipSession
-from pip.req import parse_requirements as requirements
 from setuptools import setup
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-if sys.version_info[0] == 2:
-    from codecs import open
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    with open(os.path.join(package, '__init__.py')) as f:
+        init_py = f.read()
+
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
 
 
-def parse_requirements(f):
-    return [str(r.req) for r in requirements(f, session=PipSession())]
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
 
-# Read requirements
-_requirements_file = os.path.join(BASE_DIR, 'requirements.txt')
-_tests_requirements_file = os.path.join(BASE_DIR, 'requirements-tests.txt')
-_REQUIRES = parse_requirements(_requirements_file)
-_TESTS_REQUIRES = parse_requirements(_tests_requirements_file)
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
 
-# Read description
-with open(os.path.join(BASE_DIR, 'README.rst'), encoding='utf-8') as f:
-    _LONG_DESCRIPTION = f.read()
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
 
-_CLASSIFIERS = (
-    'Development Status :: 5 - Production/Stable',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    'Natural Language :: English',
-    'Programming Language :: Python',
-    'Programming Language :: Python :: 3.5',
-    'Programming Language :: Python :: 3.6',
-    'Topic :: Software Development :: Libraries :: Python Modules',
-)
 
-_KEYWORDS = ' '.join([
-    'python',
-    'apistar',
-    'api',
-    'resource',
-    'crud',
-    'async',
-    'django',
-    'sqlalchemy',
-])
+name = 'apistar-crud'
+package_name = 'apistar_crud'
+version = get_version(package_name)
 
 setup(
-    name='apistar-crud',
-    version='0.2.0',
+    name=name,
+    version=version,
     description='API Star tools to create CRUD resources.',
-    long_description=_LONG_DESCRIPTION,
     author='José Antonio Perdiguero López',
     author_email='perdy.hh@gmail.com',
     maintainer='José Antonio Perdiguero López',
     maintainer_email='perdy.hh@gmail.com',
     url='https://github.com/PeRDy/apistar-crud',
     download_url='https://github.com/PeRDy/apistar-crud',
-    packages=[
-        'apistar_crud',
+    packages=get_packages(package_name),
+    package_data=get_package_data(package_name),
+    install_requires=[
+        'apistar',
     ],
-    include_package_data=True,
-    install_requires=_REQUIRES,
-    tests_require=_TESTS_REQUIRES,
-    extras_require={
-        'dev': [
-            'setuptools',
-            'pip',
-            'wheel',
-            'twine',
-            'bumpversion',
-            'pre-commit',
-            'clinner',
-        ] + _TESTS_REQUIRES,
-    },
+    tests_require=[
+        'apistar',
+        'apistar-sqlalchemy',
+        'aiohttp',
+        'clinner',
+        'coverage',
+        'Django',
+        'prospector',
+        'pytest',
+        'pytest-xdist',
+        'pytest-cov',
+        'SQLAlchemy',
+        'tox',
+    ],
     license='GPLv3',
-    zip_safe=False,
-    keywords=_KEYWORDS,
-    classifiers=_CLASSIFIERS,
+    keywords=' '.join([
+        'python',
+        'apistar',
+        'api',
+        'resource',
+        'crud',
+        'async',
+        'django',
+        'sqlalchemy',
+    ]),
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
