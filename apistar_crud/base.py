5cb437136cf8a8955561452f7c0d32e06c9ba11d
José Antonio Perdiguero López <perdy.hh@gmail.com>
2018-05-22 19:09:59 +0200

    Resource routes is a property instead of a static list

--- apistar_crud/base.py
+++ apistar_crud/base.py
@@ -5,25 +5,25 @@ from apistar import Route
 
 class BaseResource(type):
     METHODS = {
-        'list': ('/', 'GET'),  # List resource collection
-        'drop': ('/', 'DELETE'),  # Drop resource entire collection
-        'create': ('/', 'POST'),  # Create a new element for this resource
-        'retrieve': ('/{element_id}/', 'GET'),  # Retrieve an element of this resource
-        'update': ('/{element_id}/', 'PUT'),  # Update an element of this resource
-        'delete': ('/{element_id}/', 'DELETE'),  # Delete an element of this resource
+        "list": ("/", "GET"),  # List resource collection
+        "drop": ("/", "DELETE"),  # Drop resource entire collection
+        "create": ("/", "POST"),  # Create a new element for this resource
+        "retrieve": ("/{element_id}/", "GET"),  # Retrieve an element of this resource
+        "update": ("/{element_id}/", "PUT"),  # Update an element of this resource
+        "delete": ("/{element_id}/", "DELETE"),  # Delete an element of this resource
     }
     AVAILABLE_METHODS = tuple(METHODS.keys())
-    DEFAULT_METHODS = ('create', 'retrieve', 'update', 'delete', 'list')
+    DEFAULT_METHODS = ("create", "retrieve", "update", "delete", "list")
 
     def __new__(mcs, name, bases, namespace):
         try:
-            model = namespace['model']
-            input_type = namespace['input_type']
-            output_type = namespace['output_type']
+            model = namespace["model"]
+            input_type = namespace["input_type"]
+            output_type = namespace["output_type"]
         except KeyError as e:
             raise AttributeError('{} needs to define attribute: "{}"'.format(name, e))
 
-        methods = namespace.get('methods', mcs.DEFAULT_METHODS)
+        methods = namespace.get("methods", mcs.DEFAULT_METHODS)
 
         mcs.add_methods(namespace, methods, model, input_type, output_type)
         mcs.add_routes(namespace, methods)
@@ -32,8 +32,14 @@ class BaseResource(type):
 
     @classmethod
     def add_routes(mcs, namespace: Dict[str, Any], methods: Iterable[str]):
-        routes = [Route(*mcs.METHODS[method], namespace[method], name=method) for method in methods]
-        namespace['routes'] = routes
+
+        class Routes:
+            """Routes descriptor"""
+
+            def __get__(self, instance, owner):
+                return [Route(*mcs.METHODS[method], getattr(owner, method), name=method) for method in methods]
+
+        namespace["routes"] = Routes()
 
     @classmethod
     def add_methods(mcs, namespace: Dict[str, Any], methods: Iterable[str], model, input_type, output_type):
@@ -41,10 +47,11 @@ class BaseResource(type):
 
         for method in methods:
             try:
-                getattr(mcs, 'add_{}'.format(method))(namespace, model, input_type, output_type)
+                getattr(mcs, "add_{}".format(method))(namespace, model, input_type, output_type)
             except AttributeError:
-                raise AttributeError('Invalid method "{}", must be one of: {}.'.format(
-                    method, ', '.join(mcs.AVAILABLE_METHODS)))
+                raise AttributeError(
+                    'Invalid method "{}", must be one of: {}.'.format(method, ", ".join(mcs.AVAILABLE_METHODS))
+                )
 
     @classmethod
     def add_create(mcs, namespace: Dict[str, Any], model, input_type, output_type):
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,13 +1,12 @@
 import pytest
 from apistar import types, validators
 
-# Fake model
 from apistar_crud.sqlalchemy import Resource
 
 
 class PuppyModel:
     id = 1
-    name = 'Canna'
+    name = "Canna"
 
 
 class PuppyInputType(types.Type):
@@ -20,15 +19,19 @@ class PuppyOutputType(types.Type):
 
 
 class TestCaseBaseCRUDResource:
-    @pytest.fixture(scope='function')
+
+    @pytest.fixture(scope="function")
     def resource(self, request):
-        if hasattr(request, 'param'):
+        if hasattr(request, "param"):
+
             class PuppyResource(metaclass=Resource):
                 model = PuppyModel
                 input_type = PuppyInputType
                 output_type = PuppyOutputType
                 methods = request.param
+
         else:
+
             class PuppyResource(metaclass=Resource):
                 model = PuppyModel
                 input_type = PuppyInputType
@@ -37,50 +40,87 @@ class TestCaseBaseCRUDResource:
         return PuppyResource
 
     def test_new_default_methods(self, resource):
-        assert hasattr(resource, 'create')
-        assert hasattr(resource, 'retrieve')
-        assert hasattr(resource, 'update')
-        assert hasattr(resource, 'delete')
-        assert hasattr(resource, 'list')
+        assert hasattr(resource, "create")
+        assert hasattr(resource, "retrieve")
+        assert hasattr(resource, "update")
+        assert hasattr(resource, "delete")
+        assert hasattr(resource, "list")
         assert len(resource.routes) == 5
-        assert [route.handler for route in resource.routes] == \
-               [resource.create, resource.retrieve, resource.update, resource.delete, resource.list]
+        assert [route.handler for route in resource.routes] == [
+            resource.create,
+            resource.retrieve,
+            resource.update,
+            resource.delete,
+            resource.list,
+        ]
 
-    @pytest.mark.parametrize('resource', [('create', 'retrieve', 'update', 'delete', 'list', 'drop')],
-                             indirect=['resource'])
+    @pytest.mark.parametrize(
+        "resource", [("create", "retrieve", "update", "delete", "list", "drop")], indirect=["resource"]
+    )
     def test_new_explicit_methods(self, resource):
-        assert hasattr(resource, 'create')
-        assert hasattr(resource, 'retrieve')
-        assert hasattr(resource, 'update')
-        assert hasattr(resource, 'delete')
-        assert hasattr(resource, 'list')
-        assert hasattr(resource, 'drop')
+        assert hasattr(resource, "create")
+        assert hasattr(resource, "retrieve")
+        assert hasattr(resource, "update")
+        assert hasattr(resource, "delete")
+        assert hasattr(resource, "list")
+        assert hasattr(resource, "drop")
         assert len(resource.routes) == 6
-        assert [route.handler for route in resource.routes] == \
-               [resource.create, resource.retrieve, resource.update, resource.delete, resource.list, resource.drop]
+        assert [route.handler for route in resource.routes] == [
+            resource.create,
+            resource.retrieve,
+            resource.update,
+            resource.delete,
+            resource.list,
+            resource.drop,
+        ]
+
+    def test_overriden_method(self, resource):
+
+        class SpecializedPuppyResource(resource):
+
+            def list(self):
+                return []
+
+        assert hasattr(SpecializedPuppyResource, "create")
+        assert hasattr(SpecializedPuppyResource, "retrieve")
+        assert hasattr(SpecializedPuppyResource, "update")
+        assert hasattr(SpecializedPuppyResource, "delete")
+        assert hasattr(SpecializedPuppyResource, "list")
+        assert len(SpecializedPuppyResource.routes) == 5
+        assert [route.handler for route in SpecializedPuppyResource.routes] == [
+            resource.create,
+            resource.retrieve,
+            resource.update,
+            resource.delete,
+            SpecializedPuppyResource.list,
+        ]
 
     def test_new_no_model(self):
         with pytest.raises(AttributeError):
+
             class PuppyResource(metaclass=Resource):
                 input_type = PuppyInputType
                 output_type = PuppyOutputType
 
     def test_new_no_input_type(self):
         with pytest.raises(AttributeError):
+
             class PuppyResource(metaclass=Resource):
                 model = PuppyModel
                 output_type = PuppyOutputType
 
     def test_new_no_output_type(self):
         with pytest.raises(AttributeError):
+
             class PuppyResource(metaclass=Resource):
                 model = PuppyModel
                 input_type = PuppyInputType
 
     def test_new_wrong_methods(self):
         with pytest.raises(AttributeError):
+
             class PuppyResource(metaclass=Resource):
                 model = PuppyModel
                 input_type = PuppyInputType
                 output_type = PuppyOutputType
-                methods = ('foo',)
+                methods = ("foo",)
