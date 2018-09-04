4a1f742b7b21ff9f48796d98e0899adcf9d270aa
José Antonio Perdiguero López <perdy@perdy.io>
2018-09-04 14:43:40 +0200

    Remove webpack bundle analyzer

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,12 +4,11 @@ const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
-  plugins: [new BundleAnalyzerPlugin()],
+  plugins: [],
   optimization: {
     nodeEnv: "production",
     minimizer: [
